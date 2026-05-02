a5bbdd4c71a45b761e34821b1c7ddcefdc4e7d42
Vishakha Roman <vishakharoman@Vishakhas-MacBook-Air.local>
2026-05-02 20:50:10 +0530

    Tailwind css integradtion and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Nexora AI</h1>
-      <p>Your AI assistant</p>
-    </div>
+
+      <div
+      className="grid grid-cols-5  h-screen text-center">
+      <div className="col-span-1 bg-zinc-800" >  </div>
+      <div className=" col-span-4 p-10">
+        <div className="container h-140">
+
+        </div>
+        <div className="bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16">
+          <input className='w-full h-full p-3 outline-none' type="text" name="" placeholder="Ask me anything!"/>
+          <button>Ask</button>
+        </div>
+
+      </div>
+      </div>
   );
 }
 
